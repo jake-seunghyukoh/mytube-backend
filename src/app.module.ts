9d5fd3fe9539161dde8b49e5558073481e5c7cf1
Seunghyuk <seunghyukoh0@gmail.com>
2021-10-19 12:10:03 +0900

    Update - Add new endpoint users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { VideosModule } from './videos/videos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [VideosModule],
+  imports: [VideosModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
