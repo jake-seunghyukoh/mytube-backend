cd1f610462d6aaca097488ca81a90070457ec370
Seunghyuk <seunghyukoh0@gmail.com>
2021-10-19 17:32:22 +0900

    Update - Implement test code

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,15 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
-import { AuthController } from './auth.controller';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -26,10 +27,36 @@ describe('AuthController', () => {
       controllers: [AuthController],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('signup', () => {
+    it('should register a user', async () => {
+      const result = {
+        id: 1,
+        username: 'test',
+      };
+
+      expect(
+        await authController.signUp({ username: 'test', password: 'password' }),
+      ).toBe(result);
+    });
+
+    it("should throw an HttpException 'User Already Exists' if user already exist", async () => {
+      expect(
+        authController.signUp({
+          username: 'test',
+          password: 'password',
+        }),
+      ).rejects.toEqual(
+        new HttpException('User Already Exists', HttpStatus.BAD_REQUEST),
+      );
+    });
+
+    // Should we test things with dto and validation pipe?
   });
 });
