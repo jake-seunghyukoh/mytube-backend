bd219022972dd17bd26573a571702dd4959374ed
Seunghyuk <seunghyukoh0@gmail.com>
2021-10-20 15:59:52 +0900

    Update - Example test code with mocking prisma

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,14 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma.service';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { mockReset } from 'jest-mock-extended';
+import { prismaMock } from '../singleton';
+import { User } from '.prisma/client';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    mockReset(prismaMock);
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, PrismaService],
-      exports: [UsersService],
+      providers: [
+        UsersService,
+        { provide: PrismaService, useValue: prismaMock },
+      ],
       controllers: [UsersController],
     }).compile();
 
@@ -18,5 +24,16 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create an user', async () => {
+    const user: User = { id: 1, username: 'test', password: 'password' };
+    prismaMock.user.create.mockResolvedValue(user);
+
+    const input = { username: 'test', password: 'password' };
+
+    const result = await service.createOne(input.username, input.password);
+
+    expect(result).toBe(user);
   });
 });
