f3246203f60179c4e97f863a350fa835cf6aa91b
Seunghyuk <seunghyukoh0@gmail.com>
2021-10-20 16:50:04 +0900

    Update - Test code : Auth - Add new tests for validateUser

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -7,9 +7,14 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
+import { User } from '.prisma/client';
+import { SignUpResponseDto } from './dto/signup.dto';
+import { hash } from 'bcrypt';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -25,10 +30,69 @@ describe('AuthService', () => {
       controllers: [AuthController],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('validateUser', () => {
+    it('should validate a user', async () => {
+      const userInfo: User = {
+        id: 1,
+        username: 'username',
+        password: await hash('password', 10),
+      };
+
+      jest
+        .spyOn(usersService, 'findOne')
+        .mockImplementation(async () => userInfo);
+
+      const result: SignUpResponseDto | null = await authService.validateUser(
+        'username',
+        'password',
+      );
+      expect(result).not.toBeNull();
+      expect(result).toEqual({ id: 1, username: 'username' });
+    });
+
+    it('should return null with not existing user', async () => {
+      jest.spyOn(usersService, 'findOne').mockImplementation(async () => null);
+
+      const result: SignUpResponseDto | null = await authService.validateUser(
+        'username',
+        'password',
+      );
+      expect(result).toBeNull();
+    });
+
+    it('should return null with wrong password', async () => {
+      const userInfo: User = {
+        id: 1,
+        username: 'username',
+        password: await hash('password', 10),
+      };
+
+      jest
+        .spyOn(usersService, 'findOne')
+        .mockImplementation(async () => userInfo);
+
+      const result: SignUpResponseDto | null = await authService.validateUser(
+        'username',
+        'wrong_password!!!!',
+      );
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('login', () => {
+    it('should be defined', () => {});
+  });
+
+  describe('signUp', () => {
+    it('should be defined', () => {});
   });
 });
