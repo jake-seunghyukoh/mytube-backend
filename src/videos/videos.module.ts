b88545af5be84fa7986cfc83b83a547e85bb24c3
Seunghyuk <seunghyukoh0@gmail.com>
2021-10-21 17:03:17 +0900

    Update - Add new endpoint - UploadVideo - Save video which user send to server - Save video in directory ~/upload/

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { VideosController } from './videos.controller';
 import { VideosService } from './videos.service';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './upload',
+    }),
+  ],
   controllers: [VideosController],
   providers: [VideosService],
 })
